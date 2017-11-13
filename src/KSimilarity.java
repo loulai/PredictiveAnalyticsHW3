ba606935d38a0545506293736db942698a16a62b
Louise Lai <louiselai88@gmail.com>
2017-11-13 00:53:48 -0500

    cosine added

--- src/KSimilarity.java
+++ src/KSimilarity.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class KSimilarity {
@@ -22,7 +24,7 @@ public class KSimilarity {
 	    return dotProduct / (Math.sqrt(normA) * Math.sqrt(normB));
 	}
 	
-	public KSimilarity(File file, int k) throws FileNotFoundException {
+	public KSimilarity(File file, int topK) throws FileNotFoundException {
 		String vectorKey = String.valueOf(filenameToInt(file)-1);
 		int n = 4; //num articles to evaluate
 		TFIDF myTFIDF = new TFIDF(n); //evaluate 2 articles
@@ -32,17 +34,48 @@ public class KSimilarity {
 		ArrayList<Double> vecA = myTFIDF.columnsMap.get(vectorKey);
 		
 		//System.out.println(vecA);
-		double[] cosineValues = new double[n];
-		int a = 0;
+		ArrayList<Double> cosineValues = new ArrayList<Double>();
+		
 		for(String key : myTFIDF.columnsMap.keySet()) {
 			ArrayList<Double> vecX = myTFIDF.columnsMap.get(key);
-			cosineValues[i] = cosineSimilarity(vecA, vecX);
-			System.out.println(cosineValues[a]);
+			cosineValues.add(cosineSimilarity(vecA, vecX));
 		}
 		
-		for(int i = 0; i <cosineValues[i]; i++) {
-			
+		
+		ArrayList<Double> sortedCosine = (ArrayList<Double>) cosineValues.clone();
+		Collections.sort(sortedCosine, Collections.reverseOrder());
+		
+		System.out.println("Original cosine vals " + cosineValues);
+		System.out.println("Sorted cosine vals " + sortedCosine);
+		ArrayList<Double> topKCosines = new ArrayList<Double>();
+		int[] topKIndex = new int[topK];
+		
+		topKCosines = new ArrayList<Double>(sortedCosine.subList(1,topK+1));
+		
+		for(int i = 0; i < topK; i++) {
+			topKIndex[i] = cosineValues.indexOf(topKCosines.get(i));
 		}
+		
+		System.out.println(topKCosines);
+		System.out.println(cosineValues.indexOf(0.19883635937271754));
+		/*for(int i = 0; i < topK; i++) {
+			System.out.println(topKIndex[i]);
+		}*/
+		
+		
+		/*
+		for(int i = 0; i < cosineValues.size(); i++) {
+			System.out.printf("%d. Cosine value: %f\n", i, cosineValues[i]);
+			if(cosineValues[i] == 1 ) {
+				//the actual article
+			} else if (cosineValues[i] > maxCosine) {
+				maxCosine = cosineValues[i];
+				maxIndex = i;
+				System.out.println("maxCosine: " + maxCosine);
+				System.out.println("maxIndex : " + maxIndex);
+			}
+		}*/
+		//System.out.println(maxCosine + " " + maxIndex);
 	}
 	
 	public int filenameToInt(File file) {
